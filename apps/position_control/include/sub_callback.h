33055d41a921629333fcb7780571466940432911
aurel <appiusa@student.ethz.ch>
2022-03-24 14:42:47 +0100

    adjustet header for new messag name

--- apps/position_control/include/sub_callback.h
+++ apps/position_control/include/sub_callback.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#include "PosCmd_msg.h"
-#include "PosCmd_msgPubSubTypes.h"
+#include "QuadPosCmd_msg.h"
+#include "QuadPosCmd_msgPubSubTypes.h"
 
 #include "Header_msg.h"
 #include "Header_msgPubSubTypes.h"
 // Subscriber data that needs to be accessed in main
 namespace sub {
-cpp_msg::PosCmd_msg pos_cmd;
+cpp_msg::QuadPosCmd_msg pos_cmd;
 cpp_msg::Header_msg px4_cmd;
 } // namespace sub
 
