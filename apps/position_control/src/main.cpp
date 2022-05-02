96d8bd95316c87be2d55e0ccd837c57fb7e5bfd6
aurel <appiusa@student.ethz.ch>
2022-05-02 23:30:30 +0200

    removed old crazy homeposition set command

--- apps/position_control/src/main.cpp
+++ apps/position_control/src/main.cpp
@@ -13,16 +13,18 @@ using std::this_thread::sleep_for;
 constexpr static float x_offset = 0.5;
 constexpr static float y_offset = 0.5;
 
+// std::function<void(const mavlink_message_t &)> homePositionCallback =
+//     [](const mavlink_message_t &raw_msg)
+// {
+//   auto longitude = mavlink_msg_home_position_get_longitude(&raw_msg);
+//   std::cout << "Longitude: " << longitude << '\n';
+// };
 
-std::function<void(const mavlink_message_t &)> homePositionCallback =
-  [](const mavlink_message_t &raw_msg) {
-    auto longitude = mavlink_msg_home_position_get_longitude(&raw_msg);
-    std::cout << "Longitude: " << longitude << '\n';
-  };
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // check if serial port was given as command line argument
-  if (argc != 2) {
+  if (argc != 2)
+  {
     usage(argv[0]);
     return 1;
   }
@@ -45,13 +47,15 @@ int main(int argc, char **argv) {
                             "px4_status_msgs", dp.participant());
 
   // check connection success
-  if (connection_result != ConnectionResult::Success) {
+  if (connection_result != ConnectionResult::Success)
+  {
     std::cerr << "Connection failed: " << connection_result << '\n';
     return 1;
   }
 
   auto system = get_system(mavsdk);
-  if (!system) {
+  if (!system)
+  {
     return 1;
   }
 
@@ -59,16 +63,18 @@ int main(int argc, char **argv) {
   auto action = Action{system};
   auto offboard = Offboard{system};
   auto telemetry = Telemetry{system};
-  auto mavlinkPassthrough = MavlinkPassthrough{system};
+  // auto mavlinkPassthrough = MavlinkPassthrough{system};
   std::cout << "System is ready\n";
 
   // check matched
   for (int i = 0;
        !feedback_pub.listener.matched() || !action_cmd_sub.listener->matched();
-       ++i) {
+       ++i)
+  {
     std::cout << "Publisher hasn't matched. ";
 
-    if (i == 9) {
+    if (i == 9)
+    {
       std::cerr << "Failed to match a subscriber." << std::endl;
       return 1;
     }
@@ -82,23 +88,29 @@ int main(int argc, char **argv) {
   bool wait_for_data = true;
 
   // subscribe to home position
-  mavlinkPassthrough.subscribe_message_async(410, homePositionCallback);
+  // mavlinkPassthrough.subscribe_message_async(410, homePositionCallback);
 
   // main loop
-  while (true) {
+  while (true)
+  {
     // wait for new data?
-    if (wait_for_data) {
+    if (wait_for_data)
+    {
       std::cout << "wait for action command" << std::endl;
       action_cmd_sub.listener->wait_for_data();
       std::cout << "received an action command" << std::endl;
-    } else {
+    }
+    else
+    {
       std::cout << "skipped wait for data." << std::endl;
       // reset bool
       wait_for_data = true;
     }
 
-    switch (sub::action_cmd.action) {
-    case Action_cmd::act_status: {
+    switch (sub::action_cmd.action)
+    {
+    case Action_cmd::act_status:
+    {
       pub::feedback.feedback = FeedbackType::fb_status;
       pub::feedback.status.battery =
           (int)(telemetry.battery().remaining_percent * 100.0);
@@ -108,31 +120,36 @@ int main(int argc, char **argv) {
       std::cout << "publish status" << std::endl;
       feedback_pub.publish(pub::feedback);
       std::cout << "published status" << std::endl;
-    } break;
+    }
+    break;
 
-    case Action_cmd::act_arm: {
+    case Action_cmd::act_arm:
+    {
       const auto arm_result = action.arm();
       std::cout << arm_result << std::endl;
       pub::feedback.feedback = FeedbackType::fb_arm;
-      if (arm_result == mavsdk::Action::Result::Success) {
+      if (arm_result == mavsdk::Action::Result::Success)
+      {
         pub::feedback.result = ResultType::res_success;
-      } else {
+      }
+      else
+      {
         pub::feedback.result = ResultType::res_fail;
       }
       feedback_pub.publish(pub::feedback);
 
       /* SET HOME POSITION */
-      // set home position to current position [NOT TESTED]
-      mavlink_message_t msg;
-      mavlink_set_home_position_t home_position;
-      home_position.x = telemetry.position_velocity_ned().position.north_m;
-      home_position.y = telemetry.position_velocity_ned().position.east_m;
-      home_position.z = telemetry.position_velocity_ned().position.down_m;
-      mavlink_msg_set_home_position_encode(system->get_system_id(),
-                                           system->component_ids()[0], &msg,
-                                           &home_position);
-      // publish mavlink message
-      mavlinkPassthrough.send_message(msg);
+      // // set home position to current position [NOT TESTED]
+      // mavlink_message_t msg;
+      // mavlink_set_home_position_t home_position;
+      // home_position.x = telemetry.position_velocity_ned().position.north_m;
+      // home_position.y = telemetry.position_velocity_ned().position.east_m;
+      // home_position.z = telemetry.position_velocity_ned().position.down_m;
+      // mavlink_msg_set_home_position_encode(system->get_system_id(),
+      //                                      system->component_ids()[0], &msg,
+      //                                      &home_position);
+      // // publish mavlink message
+      // mavlinkPassthrough.send_message(msg);
 
       // flight mode code TODO
       // mavlink_set_mode_t flight_mode;
@@ -140,58 +157,80 @@ int main(int argc, char **argv) {
       // mavlink_msg_set_mode_encode(system->get_system_id(),
       // system->component_ids()[0], &msg, &flight_mode);
       // mavlinkPassthrough.send_message(msg);
-    } break;
+    }
+    break;
 
-    case Action_cmd::act_disarm: {
+    case Action_cmd::act_disarm:
+    {
       const auto disarm_result = action.disarm();
       std::cout << disarm_result << std::endl;
       pub::feedback.feedback = FeedbackType::fb_disarm;
-      if (disarm_result == mavsdk::Action::Result::Success) {
+      if (disarm_result == mavsdk::Action::Result::Success)
+      {
         pub::feedback.result = ResultType::res_success;
-      } else {
+      }
+      else
+      {
         pub::feedback.result = ResultType::res_fail;
       }
       feedback_pub.publish(pub::feedback);
-    } break;
+    }
+    break;
 
-    case Action_cmd::act_takeoff: {
+    case Action_cmd::act_takeoff:
+    {
       const auto takeoff_result = action.takeoff();
       std::cout << takeoff_result << std::endl;
       pub::feedback.feedback = FeedbackType::fb_takeoff;
-      if (takeoff_result == mavsdk::Action::Result::Success) {
+      if (takeoff_result == mavsdk::Action::Result::Success)
+      {
         pub::feedback.result = ResultType::res_success;
-      } else {
+      }
+      else
+      {
         pub::feedback.result = ResultType::res_fail;
       }
       feedback_pub.publish(pub::feedback);
-    } break;
+    }
+    break;
 
-    case Action_cmd::act_land: {
+    case Action_cmd::act_land:
+    {
       const auto land_result = action.land();
       std::cout << land_result << std::endl;
       pub::feedback.feedback = FeedbackType::fb_land;
-      if (land_result == mavsdk::Action::Result::Success) {
+      if (land_result == mavsdk::Action::Result::Success)
+      {
         pub::feedback.result = ResultType::res_success;
-      } else {
+      }
+      else
+      {
         pub::feedback.result = ResultType::res_fail;
       }
       feedback_pub.publish(pub::feedback);
-    } break;
+    }
+    break;
 
     // TODO (remove?)
-    case Action_cmd::act_hover: {
+    case Action_cmd::act_hover:
+    {
       const auto hover_result = action.hold();
       std::cout << hover_result << std::endl;
       pub::feedback.feedback = FeedbackType::fb_hover;
-      if (hover_result == mavsdk::Action::Result::Success) {
+      if (hover_result == mavsdk::Action::Result::Success)
+      {
         pub::feedback.result = ResultType::res_success;
-      } else {
+      }
+      else
+      {
         pub::feedback.result = ResultType::res_fail;
       }
       feedback_pub.publish(pub::feedback);
-    } break;
+    }
+    break;
 
-    case Action_cmd::act_offboard: {
+    case Action_cmd::act_offboard:
+    {
       // Send it once before starting offboard, otherwise it will be rejected.
       Offboard::PositionNedYaw position_msg{};
       // position_msg.north_m =
@@ -199,25 +238,30 @@ int main(int argc, char **argv) {
       // position_msg.east_m =
       // telemetry.position_velocity_ned().position.east_m;
       position_msg.down_m = -1.5f;
+      position_msg.yaw_deg = 0;
       offboard.set_position_ned(position_msg);
 
       Offboard::Result offboard_result = offboard.start();
       offboard.set_position_ned(position_msg);
 
-      while (true) {
+      while (true)
+      {
         // Blocks until new data is available?
         pos_cmd_sub.listener->wait_for_data_for_ms(200);
 
         // terminate offboard?
-        if (sub::action_cmd.action != Action_cmd::act_offboard) {
+        if (sub::action_cmd.action != Action_cmd::act_offboard)
+        {
           break;
         }
+
         position_msg.north_m = sub::pos_cmd.position.x + x_offset;
         position_msg.east_m = sub::pos_cmd.position.y + y_offset;
         position_msg.down_m =
             -sub::pos_cmd.position.z; // To account for px4 -z coordinate system
                                       // (North-East-Down)
         position_msg.yaw_deg = -sub::pos_cmd.yaw_angle;
+
         std::cout << "x:\t" << sub::pos_cmd.position.x << "\t y: \t"
                   << sub::pos_cmd.position.y << "\t z: \t"
                   << sub::pos_cmd.position.z << std::endl;
@@ -226,7 +270,8 @@ int main(int argc, char **argv) {
       offboard_result = offboard.stop();
       // don't wait for new data after offboard was terminated
       wait_for_data = false;
-    } break;
+    }
+    break;
 
     default:
       break;
